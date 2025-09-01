a51c969a9958244e8c0d5828e503d26f2ff5820a
Rovshan Ismailov <rovshan.ismailov@nordclan.com>
2025-09-01 16:00:07 +0400

    Добавил мидлвейр

--- TimeFlow/Web/Middlewares/ExceptionHandlingMiddleware.cs
+++ TimeFlow/Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,53 +1,59 @@
 ﻿using System.Net;
 using System.Text.Json;
 
-namespace Web.Middlewares
+namespace Web.Middlewares;
+
+public class ExceptionHandlingMiddleware
 {
-    public class ExceptionHandlingMiddleware
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
     {
-        private readonly RequestDelegate _next;
-        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        _next = next;
+        _logger = logger;
+    }
 
-        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    public async Task Invoke(HttpContext context)
+    {
+        try
         {
-            _next = next;
-            _logger = logger;
+            await _next(context);
         }
-
-        public async Task Invoke(HttpContext context)
+        catch (UnauthorizedAccessException ex)
         {
-            try
-            {
-                await _next(context);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                await HandleExceptionAsync(context, HttpStatusCode.Unauthorized, ex.Message);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                await HandleExceptionAsync(context, HttpStatusCode.NotFound, ex.Message);
-            }
-            catch (InvalidOperationException ex)
-            {
-                await HandleExceptionAsync(context, HttpStatusCode.BadRequest, ex.Message);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Произошла непредвиденная ошибка");
-                await HandleExceptionAsync(context, HttpStatusCode.InternalServerError, "Произошла ошибка сервера");
-            }
+            await HandleExceptionAsync(context, HttpStatusCode.Unauthorized, ex.Message);
         }
-
-        private static async Task HandleExceptionAsync(HttpContext context, HttpStatusCode statusCode, string message)
+        catch (KeyNotFoundException ex)
         {
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)statusCode;
-
-            var response = new { error = message };
-            var json = JsonSerializer.Serialize(response);
-
-            await context.Response.WriteAsync(json);
+            await HandleExceptionAsync(context, HttpStatusCode.NotFound, ex.Message);
         }
+        catch (InvalidOperationException ex)
+        {
+            await HandleExceptionAsync(context, HttpStatusCode.BadRequest, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Произошла непредвиденная ошибка");
+            await HandleExceptionAsync(context, HttpStatusCode.InternalServerError, "Произошла ошибка сервера");
+        }
+    }
+
+    /// <summary>
+    /// Формирует и отправляет клиенту красивый JSON-ответ с ошибкой 
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="statusCode"></param>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    private static async Task HandleExceptionAsync(HttpContext context, HttpStatusCode statusCode, string message)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+
+        var response = new { error = message };
+        var json = JsonSerializer.Serialize(response);
+
+        await context.Response.WriteAsync(json);
     }
 }
